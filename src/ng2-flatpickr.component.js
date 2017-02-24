0fb5a38c94a27524ee0a2dd447160d8de9988520
mezoistvan <mezoistvan1@gmail.com>
2017-02-25 00:29:19 +0100

    initial package

---
+++ src/ng2-flatpickr.component.js
@@ -0,0 +1,22 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var Ng2FlatpickrComponent = (function () {
+    function Ng2FlatpickrComponent() {
+    }
+    return Ng2FlatpickrComponent;
+}());
+Ng2FlatpickrComponent = __decorate([
+    core_1.Component({
+        selector: 'ng2-flatpickr',
+        template: 'Hello world!'
+    })
+], Ng2FlatpickrComponent);
+exports.Ng2FlatpickrComponent = Ng2FlatpickrComponent;
+//# sourceMappingURL=ng2-flatpickr.component.js.map
