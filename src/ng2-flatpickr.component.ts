a7ff55f422bfad37906c299d11800897ce088674
mezoistvan <mezoistvan1@gmail.com>
2017-03-07 02:30:37 +0100

    flatpickr now opens on click

--- src/ng2-flatpickr.component.ts
+++ src/ng2-flatpickr.component.ts
@@ -1,22 +1,52 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 require( 'flatpickr' );
 
 @Component({
 	selector: 'ng2-flatpickr', 
 	template: `
 		<div #flatpickr>
-			<input type="text">
-		</div>`
+			<input type="text" data-input>
+		</div>`,
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef( () => Ng2FlatpickrComponent ),
+			multi: true
+		}
+	]
 })
-export class Ng2FlatpickrComponent implements AfterViewInit {
+export class Ng2FlatpickrComponent implements AfterViewInit, ControlValueAccessor {
 
 	private flatpickr: object;
+
+	private defaultFlatpickrOptions: object = {
+		wrap: true,
+		clickOpens: true,
+		onChange: ( selectedDates: any ) => { this.writeValue( selectedDates ); }
+	};
 
 	@ViewChild('flatpickr')
 	flatpickrElement: any;
 
+	///////////////////////////////////
+
+	writeValue( value:any ) {
+		this.propagateChange = value;
+	}
+
+	registerOnChange( fn: any ) {
+		this.propagateChange = fn;
+	}
+
+	registerOnTouched() {}
+
+	propagateChange = ( _: any ) => {};
+
+	///////////////////////////////////
+
 	ngAfterViewInit() {
-		this.flatpickr = this.flatpickrElement.nativeElement.flatpickr( {} );
+		this.flatpickr = this.flatpickrElement.nativeElement.flatpickr( this.defaultFlatpickrOptions );
 	}	
 
 }
