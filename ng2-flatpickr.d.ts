8248ed6ec9f4a7a578af4fc7995c1c946830a89e
mezoistvan <mezoistvan1@gmail.com>
2017-10-18 12:33:27 +0200

    version to 0.1.19

--- ng2-flatpickr.d.ts
+++ ng2-flatpickr.d.ts
@@ -2,4 +2,3 @@ export * from './src/ng2-flatpickr.component';
 export * from './src/ng2-flatpickr.directive';
 export * from './src/flatpickr-event.interface';
 export * from './src/flatpickr-options.interface';
-export * from './src/flatpickr-instance';
